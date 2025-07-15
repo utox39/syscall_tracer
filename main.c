54688a701c03ca895d87d69312b6e8520f6dfd29
utox39 <francescomoccaldi39@gmail.com>
2025-07-15 16:29:51 +0200

    chore: initial commit

---
+++ main.c
@@ -0,0 +1,72 @@
+#include <errno.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ptrace.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <sys/user.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "syscalls_table.h"
+
+const char *get_syscall_name(unsigned long long syscall_num) {
+    size_t count = sizeof(syscalls_table) / sizeof(syscalls_table[0]);
+
+    for (size_t i = 0; i < count; i++) {
+        if (syscalls_table[i].number == syscall_num) {
+            return syscalls_table[i].name;
+        }
+    }
+
+    return "unknown";
+}
+
+int main(int argc, char *argv[]) {
+    pid_t tracee_pid;
+
+    tracee_pid = fork();
+    if (tracee_pid < 0) {
+        perror("cannot create a tracee process.\n");
+        exit(EXIT_FAILURE);
+    } else if (tracee_pid == 0) {
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        execvp(argv[1], &argv[1]);
+    }
+
+    printf("\n--------------\n\n");
+
+    int status;
+    bool in_syscall = false;
+    struct user_regs_struct regs;
+
+    // Skip the first syscall (execve)
+    waitpid(tracee_pid, &status, 0);
+    ptrace(PTRACE_SYSCALL, tracee_pid, NULL, NULL);
+
+    while (1) {
+        waitpid(tracee_pid, &status, 0);
+        if (WIFEXITED(status)) {
+            break;
+        }
+
+        ptrace(PTRACE_GETREGS, tracee_pid, NULL, &regs);
+
+        if (in_syscall == false) {
+            printf("syscall: %s [%lld] (", get_syscall_name(regs.orig_rax),
+                   regs.orig_rax);
+            printf("0x%llx, 0x%llx, 0x%llx, 0x%llx, 0x%llx, 0x%llx", regs.rdi,
+                   regs.rsi, regs.rdx, regs.r10, regs.r8, regs.r9);
+            printf(")\n");
+            in_syscall = true;
+        } else {
+            printf(" -> return = 0x%llx\n\n", regs.rax);
+            in_syscall = false;
+        }
+
+        ptrace(PTRACE_SYSCALL, tracee_pid, NULL, NULL);
+    }
+
+    return 0;
+}
